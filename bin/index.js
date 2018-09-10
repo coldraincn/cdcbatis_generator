4288d8e1bf68476d5e0d71e95225cdc375e33e90
coldraincn <handan213@gmail.com>
2018-09-10 16:19:07 +0800

    添加交互

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const program = require("commander")
+var inquirer = require('inquirer')
 const mysql = require("mysql2/promise")
 const mkdirsSync = require('../lib/utils/createDir')
 const createPo = require('./createPo')
@@ -20,9 +21,35 @@ console.log(servicedir);
 var controllerdir = mkdirsSync('./controller', 0777);
 console.log(controllerdir);
 //console.log("fdfdfdfdfdf:"+stringUtil.firstLowerCase('AaddddD'))
-
-let bundleName='com.juluancj.jlcj'
-let prefix='jl'
+let questions = [
+    {
+        type: 'input',
+        name: 'host',
+        message: 'enter mysql host?',
+    },
+    {
+        type: 'input',
+        name: 'port',
+        message: 'enter mysql port?',
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: 'enter mysql user?',
+    },
+    {
+        type: 'input',
+        name: 'password',
+        message: 'enter mysql password?',
+    },
+    {
+        type: 'input',
+        name: 'database',
+        message: 'enter mysql database?',
+    }
+]
+let bundleName = 'com.juluancj.jlcj'
+let prefix = 'jl'
 
 
 program
@@ -35,54 +62,41 @@ program
 program.command('list')
     .description('list database')
     .action(async function () {
-        console.log('host:'+program.host)
-        console.log('user:'+program.user)
-        console.log('password:'+program.password)
-        console.log('port:'+program.port)
-        console.log('database:'+program.database)
-        const connection=await mysql.createConnection({     
-            host     : program.host,       
-            user     : program.user,              
-            password : program.password,       
-            port: program.port,                   
-            database: program.database, 
-          })
-          console.log('host:'+program.host)
-          console.log('user:'+program.user)
-          console.log('password:'+program.password)
-          console.log('port:'+program.port)
-          console.log('database:'+program.database)
-        // const connection = await mysql.createConnection({
-        //     host: '127.0.0.1',
-        //     user: 'root',
-        //     password: 'jlcj1234',
-        //     port: '3306',
-        //     database: 'jlcj',
-        // })
+        let answers=await inquirer.prompt(questions)
+        console.log(answers)
+        const connection = await mysql.createConnection({
+            host: answers.host,
+            user: answers.user,
+            password: answers.password,
+            port: answers.port,
+            database: answers.database,
+        })
+      
+      
         //获取表
         let tables = await connection.query('show table status;')
         //获取表中字段
-        for(let table of tables[0]){
+        for (let table of tables[0]) {
             let cloumns = await connection.query(`show columns from ${table.Name};`)
-             //创建po
-        createPo('./entity/po/', bundleName, prefix, cloumns[0], table.Name)
-        //创建provider
-        createProvider('./mapper/provider/', bundleName, prefix, cloumns[0], table.Name)
-        //创建mapper
-        createMapper('./mapper/', bundleName, prefix, cloumns[0], table.Name)
-        //创建service
-        createService('./service/', bundleName, prefix, cloumns[0], table.Name)
-        //创建serviceImpl
-        createServiceImpl('./service/impl/', bundleName, prefix, cloumns[0], table.Name)
-        //创建controller
-        createController('./controller/', bundleName, prefix, cloumns[0], table.Name)
+            //创建po
+            createPo('./entity/po/', bundleName, prefix, cloumns[0], table.Name)
+            //创建provider
+            createProvider('./mapper/provider/', bundleName, prefix, cloumns[0], table.Name)
+            //创建mapper
+            createMapper('./mapper/', bundleName, prefix, cloumns[0], table.Name)
+            //创建service
+            createService('./service/', bundleName, prefix, cloumns[0], table.Name)
+            //创建serviceImpl
+            createServiceImpl('./service/impl/', bundleName, prefix, cloumns[0], table.Name)
+            //创建controller
+            createController('./controller/', bundleName, prefix, cloumns[0], table.Name)
         }
-        
 
-       
+
+
         // console.log(data2[0])
     })
 
 program.parse(process.argv);
 
-console.log('use:  --help')
+//console.log('use:  --help')
